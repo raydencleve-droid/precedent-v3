3357b543135400fcac54181380b4dafcebd900ec
raydencleve-droid <raydencleve@gmail.com>
2026-01-07 02:00:33 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/precedent-v3/",
+    plugins: [react()],
+    base: "/precedent-v3/",
+    server: {
+        proxy: {
+            "/api": "http://localhost:5174",
+        },
+    },
 });
