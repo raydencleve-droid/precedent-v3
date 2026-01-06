f25c0f1fdc9f3686019d4a113619a4d6c41f5c4a
raydencleve-droid <raydencleve@gmail.com>
2026-01-07 01:49:45 +0700

    Add base path to Vite configuration
    Updated Vite configuration to include base path.

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            "/api": "http://localhost:5174",
-        },
-    },
+  plugins: [react()],
+  base: "/precedent-v3/",
 });
